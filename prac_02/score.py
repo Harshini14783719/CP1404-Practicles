75799a16654e7c68f9bfc951055d37a59e4ef42c
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-06-01 14:10:45 +0800

    main and determine score function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,16 +1,27 @@
+import random
+
 
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 PASSABLE_SCORE = 50
 EXCELLENT_SCORE = 90
-score = float(input("Enter score: "))
-if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-    result = "Invalid score"
-elif score >= EXCELLENT_SCORE:
-    result = "Excellent"
-elif score >= PASSABLE_SCORE:
-    result = "Passable"
-else:
-    result = "bad"
 
-print(result)
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score(score)
+
+    print(result)
+
+
+def determine_score(score):
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        result = "Invalid score"
+    elif score >= EXCELLENT_SCORE:
+        result = "Excellent"
+    elif score >= PASSABLE_SCORE:
+        result = "Passable"
+    else:
+        result = "bad"
+    return result
+
