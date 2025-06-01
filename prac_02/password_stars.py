8f5659d089464b1416e450be307ac9bb656c3388
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-06-01 12:15:15 +0800

    get valid password

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,3 +4,11 @@ MINIMUM_LENGTH = 2
 def main():
     password = get_valid_password()
     print_asterisks(password)
+
+
+def get_valid_password():
+    password = str(input("Enter password: "))
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid Password")
+        password = str(input("Enter password: "))
+    return password
