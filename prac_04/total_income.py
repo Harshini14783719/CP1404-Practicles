67aaadb10f438ae7cc41eae8dea0d9ecd74a2126
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-06-15 02:01:27 +0800

    created function for report

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -9,10 +9,14 @@ def main():
     incomes = []
     number_of_months = int(input("How many months? "))
 
-    for month in range(1, number_of_months+ 1):
-        income = float(input(f"Enter income for month {str(number_of_months)}: "))
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
