05207045f29c21af711287e1db85b5583308b20c
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-06-21 01:04:49 +0800

    created dictionary in pep8 format

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+NAME_TO_COLOR_CODE = {
+    "ALICE BLUE": "#f0f8ff",
+    "AMETHYST": "#9966cc",
+    "ANTIQUEWHITE3": "#cdc0b0",
+    "AQUA": "#00ffff",
+    "BAKER MILLER PINK": "#ff91af",
+    "CADETBLUE1": "#98f5ff",
+    "DARK LAVENDER": "#734f96",
+    "FLORALWHITE": "#fffaf0",
+    "GOLDEN POPPY": "#fcc200",
+    "ICEBERG": "#71a6d2"
+}
+
+
