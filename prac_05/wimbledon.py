69f2946152f2e4145b116320b405a39547c9ce25
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-06-22 15:17:50 +0800

    function read_wimbledon_data

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,8 +1,26 @@
 """
 wimbledon.py
 
+Write a program to read this file, process the data and display processed information:
+the champions and how many times they have won.
+the countries of the champions in alphabetical order
+
 Estimated time: 30 minutes
 Actual time:
 """
 
 FILENAME = "wimbledon.csv"
+
+def main():
+
+
+
+def read_wimbledon_data(filename):
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        records = []
+        for line in in_file:parts = line.strip().split(",")
+            champion = parts[2]
+            country = parts[1]
+            records.append([champion, country])
+        return records
