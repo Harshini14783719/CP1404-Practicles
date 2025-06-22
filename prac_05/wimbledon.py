bc95b53fba144282fc995111bcbb36bfa60d0f78
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-06-22 15:29:20 +0800

    main function completed and final code doe with checking

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,25 +6,31 @@ the champions and how many times they have won.
 the countries of the champions in alphabetical order
 
 Estimated time: 30 minutes
-Actual time:
+Actual time: 25 minutes
 """
 
 FILENAME = "wimbledon.csv"
 
+
 def main():
     records = read_wimbledon_data(FILENAME)
     champions_to_wins = count_champions(records)
-    
+    countries = get_countries(records)
 
+    print("Wimbledon Champions:")
+    for champion, wins in champions_to_wins.items():
+        print(f"{champion} {wins}")
 
-
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(countries))
 
 
 def read_wimbledon_data(filename):
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         records = []
-        for line in in_file:parts = line.strip().split(",")
+        for line in in_file:
+            parts = line.strip().split(",")
             champion = parts[2]
             country = parts[1]
             records.append([champion, country])
@@ -36,3 +42,10 @@ def count_champions(records):
     for champion, _ in records:
         champion_to_wins[champion] = champion_to_wins.get(champion, 0) + 1
     return champion_to_wins
+
+
+def get_countries(records):
+    return {country for _, country in records}
+
+
+main()
