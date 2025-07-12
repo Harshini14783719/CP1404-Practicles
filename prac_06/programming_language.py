fb8312bee621ccf031149cb136c86f8c7b63b998
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-12 22:45:36 +0800

    created class ProgrammingLanguage and initialized the attributes

---
+++ prac_06/programming_language.py
@@ -0,0 +1,8 @@
+class ProgramingLanguage:
+
+    def __init__(self, name="", typing="", is_reflection=False, year=0):
+        """Initialize the attributes"""
+        self.name = name
+        self.typing = typing
+        self.is_reflection = is_reflection
+        self.year = year
