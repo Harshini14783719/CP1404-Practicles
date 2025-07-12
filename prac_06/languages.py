02da69a6b1a5131af6f5f36ddfbbfab966b0cc42
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-13 01:39:38 +0800

    final code

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -7,14 +7,14 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-    print(python)
-    print(visual_basic)
-    print(ruby)
+    #print(python)
 
     languages = [ruby, python, visual_basic]
 
     print(f"The dynamically typed languages are: ")
     dynamic_language = [language.name for language in languages if language.is_dynamic()]
     print(dynamic_language)
+    for language in dynamic_language:
+        print(language)
 
 main()
