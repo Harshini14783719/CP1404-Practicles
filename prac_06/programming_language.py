43987fc8e74b334a2bd3b36358c235ee2ed06f41
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-13 01:32:03 +0800

    created string function to convert object to string in output

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,4 +1,4 @@
-class ProgramingLanguage:
+class ProgrammingLanguage:
 
     def __init__(self, name="", typing="", is_reflection=False, year=0):
         """Initialize the attributes"""
@@ -10,3 +10,7 @@ class ProgramingLanguage:
     def is_dynamic(self):
         """It will return True when typing is dynamic"""
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        """convert object as a string for output"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.is_reflection}, First appeared in {self.year}"
