b1606043f9f4736e1a0903f2004b161aa8ca26bc
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-17 22:11:31 +0800

    function filter_projects completed

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -20,15 +20,22 @@ Q - Quit"""
 
 def main():
     print("Welcome to Pythonic Project Management")
+    projects = load_projects(FILENAME)
+    print(f"Loaded {len(projects)} projects from {FILENAME}.")
     print(MENU)
     choice = input(">>> ").upper()
+
     while choice != "Q":
         if choice == "L":
-            load_projects(FILENAME)
+            filename = input("Filename: ")
+            projects = load_projects(filename)
         elif choice == "S":
-            
-
-
+            filename = input("Filename to save to: ")
+            save_projects(filename,projects)
+        elif choice == "D":
+            display_projects(projects)
+        elif choice == "F":
+            filter_projects()
 
 
 def load_projects(filename):
@@ -38,3 +45,27 @@ def load_projects(filename):
             name, start_date, priority, cost_estimate,completion_percentage = line.strip().split(',')
             projects.append(Project(name, datetime.strptime(start_date, "%d/%m/%Y").date(), int(priority), float(cost_estimate), int(cost_estimate)))
     return projects
+
+
+def save_projects(filename, projects):
+    with open(filename, "w") as out_file:
+        for project in projects:
+            print(f"{project.name}, {project.start_date}, {project.priority}, {project.cost_estimate}, {project.completion_percentage}", file=out_file)
+
+
+def display_projects(projects):
+    for i, project in enumerate(projects):
+        print(project)
+
+
+def filter_projects(projects):
+    date_string = input("Enter date to show projects after that date (dd/mm/yyyy): ")
+    try:
+        filter_date = datetime.strptime(date_string, "%d/%m/%Y").date()
+    except ValueError:
+        print("Invalid date format.")
+        return
+    filtered_projects = [project for project in projects if project.start_date >= filter_date]
+    filtered_projects.sort()
+    for project in filtered_projects:
+        print(project)
