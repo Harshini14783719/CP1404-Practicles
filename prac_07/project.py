93dbad985d6bd8e07575e6e7555c3ff8cb7182ec
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-17 16:59:20 +0800

    initialised the attributes

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
+
 class Project:
 
-    def __init__(self,):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
 
-
+def
