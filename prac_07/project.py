ed93dc9386bf5ef461e74e705e7eb69a2af7a2b2
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-17 17:09:55 +0800

    function is complete to return True if complete

--- prac_07/project.py
+++ prac_07/project.py
@@ -10,7 +10,15 @@ class Project:
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
 
-def __str__(self):
-    return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
-            f"priority: {self.priority}, estimate: ${self.cost_estimate:,.2f}, "
-            f"completion: {self.completion_percentage}%")
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority: {self.priority}, estimate: ${self.cost_estimate:,.2f}, "
+                f"completion: {self.completion_percentage}%")
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def is_complete(self):
+        return self.completion_percentage == 100
+
+    def update
