fbc2f3fe57c4716208054e9070889c2842dfc5e0
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-20 23:00:44 +0800

    created class

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,3 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
+class DynamicLabelsApp(App):
+    def __init__(self):
+        super().__init__()
+        self.names = ["Alice", "Bob", "Charlie", "Diana", "Eve"]
