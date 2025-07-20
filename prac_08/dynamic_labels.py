9686c57ef6417a1c8ef4499963fc535fee66a7ab
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-20 23:01:33 +0800

    created build and for loop in build for labels

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,7 +2,18 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
+
 class DynamicLabelsApp(App):
+
     def __init__(self):
         super().__init__()
         self.names = ["Alice", "Bob", "Charlie", "Diana", "Eve"]
+
+    def build(self):
+        self.root = Builder.load_file('dynamic_labels.kv')
+
+        for name in self.names:
+            label = Label(text=name, font_size=24)
+            self.root.ids.main.add_widget(label)
+
+        return self.root
