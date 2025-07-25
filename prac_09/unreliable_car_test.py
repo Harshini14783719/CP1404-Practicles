dcca2a7adb646bfa76ea7934c57780bf29ee2e0c
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-26 00:01:33 +0800

    completed code

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -10,8 +10,20 @@ def main():
     unreliable_successes = 0
     tests = 100
 
-    for i in range:
-        if reliable_car(1) > 0:
+    for i in range(tests):
+        if reliable_car.drive(1) > 0:
             reliable_successes += 1
-        if unreliable_car(1) > 0:
+        if unreliable_car.drive(1) > 0:
             unreliable_successes += 1
+
+    if reliable_successes > 70:
+        print("reliable car passed test")
+    else:
+        print("Reliable car failed the test")
+
+    if unreliable_successes < 30:
+        print("Unreliable car passed the test.")
+    else:
+        print("Unreliable car failed the test.")
+
+main()
