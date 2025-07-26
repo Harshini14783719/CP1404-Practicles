e6530481784d7a9631755dc44938c4155b46bee2
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-27 00:09:57 +0800

    changed the calculation method for price per km

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,10 +8,10 @@ class SilverServiceTaxi(Taxi):
     def __init__(self, name="", fuel=0, fanciness=0.0):
         super().__init__(name, fuel)
         self.fanciness = fanciness
-        self.price_per_km = Taxi.price_per_km * fanciness
+        self.price_per_km *= fanciness
 
     def __str__(self):
         return f"{super().__str__()}, plus flagfall of {self.flagfall}"
-    
+
     def get_fare(self):
         return super().get_fare() + self.flagfall
