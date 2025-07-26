5a7ad0970eb049e7874c3f27da1c4dc30734773e
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-26 19:30:51 +0800

    completed coode

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,3 +9,9 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        return f"{super().__str__()}, plus flagfall of {self.flagfall} "
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
