c96739cb22d15a11718568a47dd2437310a96d1e
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-26 19:15:31 +0800

    added class variable flafile

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,6 +2,10 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+
+    flagfile = 4.50
+
     def __init__(self, name="", fuel=0, fanciness=0.0):
         super().__init__(name, fuel)
         self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
