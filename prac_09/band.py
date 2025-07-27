d854820a564703b097cffff5c057f3699d066e35
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-07-27 15:12:35 +0800

    added musician

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,4 +1,12 @@
 class Band:
     def __init__(self, name=""):
+        """Initialize a Band with a name and empty list of musicians."""
         self.name = name
         self.members = []
+
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.members.append(musician)
+
+    def __str__(self):
+        
