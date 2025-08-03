271f02643d27ea7f15393fa369c63859175e1b4a
priyanka <harshini.raghuram@my.jcu.edu.au>
2025-08-03 18:45:30 +0800

    completed code before testing with sample output

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,4 +8,17 @@ def main():
         if not query:
             print("Thank you.")
 
-        
+        try:
+            # Attempt to fetch page details
+            page = wikipedia.page(query, auto_suggest=False)
+            print(f"\n{page.title}")
+            print(wikipedia.summary(query, auto_suggest=False))
+            print(page.url)
+        except wikipedia.exceptions.DisambiguationError as e:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(e.options[:10])  # Show first 10 options
+        except wikipedia.exceptions.PageError:
+            print('Page id "{}" does not match any pages. Try another id!'.format(query))
+
+
+main()
